1dd4ef289c3d91d1fda5fdf343563ecfa82f2f83
Hanh Nguyen <hanh.cho.do@gmail.com>
2022-01-16 17:36:35 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="test">
+    testtttttttttttttt
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    ssr: false,
+    buildModules: [],
+    css: ['assets/app.css'],
+})
